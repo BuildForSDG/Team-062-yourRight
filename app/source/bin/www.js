d0e16fff854fa9eceb8b3287e056844a815d4c9b
unknown <infinity_michael@yahoo.com>
2020-05-06 03:29:52 -0700

    fix:2nd commit for codacy linting'

--- app/source/bin/www.js
+++ app/source/bin/www.js
@@ -39,23 +39,23 @@ const normalizePort = (val) => {
   return false;
 };
 
-// const terminate = (server, options = { coredump: false, timeout: 500 }) => {
-//   // Exit function
-//   // const exit = (code) => {
-//   //   options.coredump ? process.abort() : process.exit(code);
-//   // };
+const terminate = (server, options = { coredump: false, timeout: 500 }) => {
+  // Exit function
+  // const exit = (code) => {
+  //   options.coredump ? process.abort() : process.exit(code);
+  // };
 
-//   return (code, reason) => (err) => {
-//     if (err && err instanceof Error) {
-//       // Log error information, use a proper logging library here :)
-//       // console.log(err.message, err.stack);
-//     }
+  return (code, reason) => (err) => {
+    // if (err && err instanceof Error) {
+    //   // Log error information, use a proper logging library here :)
+    //   // console.log(err.message, err.stack);
+    // }
 
-//     // Attempt a graceful shutdown
-//     server.close(exit);
-//     setTimeout(exit, options.timeout).unref();
-//   };
-// };
+    // Attempt a graceful shutdown
+    server.close(exit);
+    setTimeout(exit, options.timeout).unref();
+  };
+};
 
 const port = normalizePort(env.appPort || '5000');
 
